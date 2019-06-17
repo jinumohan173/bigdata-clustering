eea08815ddb95255d99574db919b654ab0ade581
jinu mohan <32955078+jinumohan173@users.noreply.github.com>
2019-06-17 08:13:48 +0530

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,32 @@
+
+from tkinter import *
+from k-means.print_test import print_test
+
+
+window = Tk()
+
+window.title("Welcome to LikeGeeks app")
+
+window.geometry('350x200')
+
+lbl = Label(window, text="Hello")
+
+lbl.grid(column=0, row=0)
+
+txt = Entry(window, width=10)
+
+txt.grid(column=1, row=0)
+
+
+def clicked():
+    res = "Welcome to " + txt.get()
+    print_test()
+
+    lbl.configure(text=res)
+
+
+btn = Button(window, text="Click Me", command=clicked)
+
+btn.grid(column=2, row=0)
+
+window.mainloop()

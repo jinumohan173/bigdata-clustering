28aa592363bfdb89b7d431b9d6ea481667421eb7
jinu mohan <32955078+jinumohan173@users.noreply.github.com>
2019-06-17 18:07:32 +0530

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,15 +1,16 @@
 
 from tkinter import *
-from k-means.print_test import print_test
+from k_means import plot_function
+
 
 
 window = Tk()
 
-window.title("Welcome to LikeGeeks app")
+window.title("Big Data Clustering")
 
 window.geometry('350x200')
 
-lbl = Label(window, text="Hello")
+lbl = Label(window, text="Enter number of cluster ")
 
 lbl.grid(column=0, row=0)
 
@@ -18,15 +19,17 @@ txt = Entry(window, width=10)
 txt.grid(column=1, row=0)
 
 
-def clicked():
-    res = "Welcome to " + txt.get()
-    print_test()
+btn = Button(window, text="Generate", command=clicked)
 
+btn.grid(column=2, row=0)
+
+
+def clicked():
+    res = "number of cluster " + txt.get()+" "
+    data =  txt.get()
+    plot_function(data)
     lbl.configure(text=res)
 
 
-btn = Button(window, text="Click Me", command=clicked)
-
-btn.grid(column=2, row=0)
 
 window.mainloop()
